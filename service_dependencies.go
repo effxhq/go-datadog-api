c10c9738d3f19533109dbb927a54c9746056c830
Daniel McSheehy <dsm140130@utdallas.edu>
2020-08-03 14:17:47 -0700

    formatting

--- service_dependencies.go
+++ service_dependencies.go
@@ -6,29 +6,27 @@
  * Copyright 2013 by authors and contributors.
  */
 
- package datadog
+package datadog
 
- import (
-	 "encoding/json"
-	 "strconv"
- )
- 
+import (
+	"strconv"
+)
+
 type DependencyMap struct {
 	Calls []string `json:"calls"`
 }
- 
+
 func (client *Client) GetServiceMap(env string, startTime *int, endTime *int) (*map[string]DependencyMap, error) {
-	 uri := "/v1/service_dependencies?" + env
-	 dependencies := &map[string]DependencyMap{}
- 
-	 if startTime != nil && endTime != nil {
-		 uri += "&start=" + strconv.Itoa(*startTime) + "&end=" + strconv.Itoa(*endTime)
-	 }
- 
-	 err := client.doJsonRequest("GET", uri, nil, dependencies); err != nil {
-		 return dependencies, err
-	 }
- 
-	 return dependencies, nil
+	uri := "/v1/service_dependencies?" + env
+	dependencies := &map[string]DependencyMap{}
+
+	if startTime != nil && endTime != nil {
+		uri += "&start=" + strconv.Itoa(*startTime) + "&end=" + strconv.Itoa(*endTime)
+	}
+
+	if err := client.doJsonRequest("GET", uri, nil, dependencies); err != nil {
+		return dependencies, err
+	}
+
+	return dependencies, nil
 }
- 
