a5b4b2a182872c8691988219d43954211c37d5f2
Daniel McSheehy <dsm140130@utdallas.edu>
2020-08-03 13:51:18 -0700

    forgot to push

--- service_dependencies.go
+++ service_dependencies.go
@@ -6,29 +6,29 @@
  * Copyright 2013 by authors and contributors.
  */
 
-package datadog
+ package datadog
 
-import (
-	"encoding/json"
-	"strconv"
-)
-
-type ServiceDependencies struct {
-	ServiceName string
-	Dependencies 	[]string
+ import (
+	 "encoding/json"
+	 "strconv"
+ )
+ 
+ type DependencyMap struct {
+	Calls []string `json:"calls"`
 }
-
-func (client *Client) GetServiceMap(env string, startTime *int, endTime *int) (&map[string]DependencyMap{}, error) {
-	uri := "/v1/service_dependencies?" + env
-	dependencies := &map[string]DependencyMap{}
-
-	if startTime != nil && endTime != nil {
-		uri += "&start=" + strconv.Itoa(*startTime) + "&end=" + strconv.Itoa(*endTime)
-	}
-
-	err := client.doJsonRequest("GET", uri, nil, dependencies); err != nil {
-		return dependencies, err
-	}
-
-	return dependencies, nil
-}
+ 
+ func (client *Client) GetServiceMap(env string, startTime *int, endTime *int) (&map[string]DependencyMap{}, error) {
+	 uri := "/v1/service_dependencies?" + env
+	 dependencies := &map[string]DependencyMap{}
+ 
+	 if startTime != nil && endTime != nil {
+		 uri += "&start=" + strconv.Itoa(*startTime) + "&end=" + strconv.Itoa(*endTime)
+	 }
+ 
+	 err := client.doJsonRequest("GET", uri, nil, dependencies); err != nil {
+		 return dependencies, err
+	 }
+ 
+	 return dependencies, nil
+ }
+ 
