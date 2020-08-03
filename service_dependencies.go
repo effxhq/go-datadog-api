2cea54d81447e70dbbbf9eb7bf62ebff14efdb0d
Daniel McSheehy <dsm140130@utdallas.edu>
2020-08-03 13:41:59 -0700

    looks good to me

---
+++ service_dependencies.go
@@ -0,0 +1,35 @@
+/*
+ * Datadog API for Go
+ *
+ * Please see the included LICENSE file for licensing information.
+ *
+ * Copyright 2013 by authors and contributors.
+ */
+
+ package datadog
+
+ import (
+	 "encoding/json"
+	 "strconv"
+ )
+ 
+ type ServiceDependencies struct {
+	 ServiceName string
+	 Dependencies 	[]string
+ }
+ 
+ func (client *Client) GetServiceMap(env string, startTime *int, endTime *int) (&map[string]DependencyMap{}, error) {
+	 uri := "/v1/service_dependencies?" + env
+	 dependencies := &map[string]DependencyMap{}
+ 
+	 if startTime != nil && endTime != nil {
+		 uri += "&start=" + strconv.Itoa(*startTime) + "&end=" + strconv.Itoa(*endTime)
+	 }
+ 
+	 err := client.doJsonRequest("GET", uri, nil, dependencies); err != nil {
+		 return dependencies, err
+	 }
+ 
+	 return dependencies, nil
+ }
+ 
